9d5f48482eb399267b3e085cab87e3e45fd836bc
itham <165036944+ithamayu@users.noreply.github.com>
2024-05-28 18:52:45 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d0345f91-1d86-4a5e-a74b-701884c6ca56/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-9fe513a7-5be5-4d8c-81ff-292e724911e3/)
 
 
 echo "$curl_output" >> Update.tsx
